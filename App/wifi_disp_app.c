72e902540c184889dc1bde318aef9b922e237d64
root <root@cubieboard2.(none)>
2014-08-15 05:35:51 +0000

    rm bugs

--- App/wifi_disp_app.c
+++ App/wifi_disp_app.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -6,9 +7,10 @@
 #include <stdlib.h>
 
 #include <fcntl.h>
-#define width  320   
-#define height 240
+#define width  854
+#define height 480
 #define PORT   8090
+#define PKG_MAX 50000L
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned long u32;
@@ -39,7 +41,7 @@ int main()
   memset(&server_addr,0,sizeof(server_addr));
   server_addr.sin_family=AF_INET;
   server_addr.sin_port=htons(PORT);
-  server_addr.sin_addr.s_addr=inet_addr("192.168.123.33");
+  server_addr.sin_addr.s_addr=inet_addr("192.168.1.101");
 
   fp=open("/dev/miao",O_RDONLY);
   if(fp<0)
@@ -59,32 +61,34 @@ int main()
       {
 	  hand_addr[1]=1;//start flag
 	  hand_addr[2]=index;
-	  printf("buf=%d,%d,%d,%d",hand_addr[24],hand_addr[25],hand_addr[26],hand_addr[27]);
-	  for(i=0;i+30000<get_cnt;i+=30000)
+	  //	  printf("buf=%d,%d,%d,%d",hand_addr[24],hand_addr[25],hand_addr[26],hand_addr[27]);
+	  for(i=0;i+PKG_MAX<get_cnt;i+=PKG_MAX)
 	  {
-	    hand_addr[i]=i/30000;
+	    hand_addr[i]=i/PKG_MAX;
 	    if(i!=0)
 	    hand_addr[i+1]=0;
 	    hand_addr[i+2]=index;
-	    fb_send(hand_addr+i,30004);
+	    fb_send(hand_addr+i,PKG_MAX+4);
 	    //fb_send(hand_addr+i,5);
+
+	    usleep(80000);
 	  }
 	  hand_addr[i+1]=2;//end flag
 	  hand_addr[i+2]=index;
-	  hand_addr[i]=i/30000;
+	  hand_addr[i]=i/PKG_MAX;
 
 	index++;
 	  fb_send(hand_addr+i,get_cnt-i+4);
 		  //fb_send(hand_addr+i,5);
       }
 
-      printf("get_cnt=%ld\n",get_cnt);
-      printf("x=%d",fb_info.x);
-      printf("y=%d",fb_info.y);
-      printf("right=%d",fb_info.right);
-      printf("bottom=%d",fb_info.bottom);
-      printf("line_width=%d",fb_info.line_width);
-      printf("color%d",fb_info.fb_buf[100]);
+      //      printf("get_cnt=%ld\n",get_cnt);
+      //      printf("x=%d",fb_info.x);
+      //      printf("y=%d",fb_info.y);
+      //      printf("right=%d",fb_info.right);
+      //      printf("bottom=%d",fb_info.bottom);
+      //      printf("line_width=%d",fb_info.line_width);
+      //      printf("color%d",fb_info.fb_buf[100]);
     }
   close(fp);
   return 0;
